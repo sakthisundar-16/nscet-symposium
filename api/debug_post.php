677535153e44dda8f07f9818fcdea0d13032b824
Sakthisundar <sakthisundar-16@users.noreply.github.com>
2026-07-07 20:16:35 +0530

    Cleanup: remove unused files (old PHP, test scripts, debug files, legacy API) + add one-click deploy.ps1

--- api/debug_post.php
+++
@@ -1,2 +0,0 @@
-<?php header('Content-Type: application/json'); echo json_encode(['post'=>
-    \\, 'files'=>\\\]); ?>
--- test_admins.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'includes/db.php';
-$db = Database::getInstance()->getConnection();
-$stmt = $db->query('SELECT * FROM admins');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
